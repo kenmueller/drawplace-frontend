e76986002f8073f501ff6fe9b4729322bb83c7ad
Ken Mueller <kenmueller0@gmail.com>
2020-10-29 23:02:07 -0700

    draws lines

--- models/Place.ts
+++ models/Place.ts
@@ -1,32 +1,75 @@
 import IO from 'socket.io-client'
 
-import Cursor from './Cursor'
+import Coordinate from './Coordinate'
+import Line from './Line'
+import MouseEventCallback from './MouseEventCallback'
 
 export default class Place {
 	private context: CanvasRenderingContext2D
-	
 	private io?: SocketIOClient.Socket
-	private onMouseMoveCallback?: (event: MouseEvent) => void
+	private cursor?: Coordinate
+	private isDrawing = false
+	private lines: Line[] = []
+	
+	private onMouseDown?: MouseEventCallback
+	private onMouseMove?: MouseEventCallback
+	private onMouseUp?: MouseEventCallback
 	
 	constructor(private canvas: HTMLCanvasElement) {
 		this.context = canvas.getContext('2d')
 		this.io = IO(process.env.NEXT_PUBLIC_API_BASE_URL)
 		
-		this.io.on('cursors', (cursors: Cursor[]) => {
+		this.io.on('cursors', (cursors: Coordinate[]) => {
 			this.clear()
-			cursors.forEach(this.drawCursor)
+			this.drawLines()
+			this.drawCursors(cursors)
 		})
 		
-		this.onMouseMoveCallback = ({ clientX: x, clientY: y }) => {
-			this.io.emit('cursor', { x, y })
+		this.io.on('lines', (lines: Line[]) => {
+			this.lines = lines
+			this.drawLines()
+		})
+		
+		this.io.on('line', (line: Line) => {
+			this.lines.push(line)
+			this.drawLine(line)
+		})
+		
+		this.onMouseDown = ({ offsetX: x, offsetY: y }) => {
+			this.cursor = { x, y }
+			this.isDrawing = true
 		}
 		
-		this.canvas.addEventListener('mousemove', this.onMouseMoveCallback)
+		this.onMouseMove = ({ offsetX: x, offsetY: y }) => {
+			if (this.isDrawing) {
+				const line: Line = {
+					from: { x: this.cursor.x, y: this.cursor.y },
+					to: { x, y }
+				}
+				
+				this.drawLine(line)
+				this.lines.push(line)
+				this.io.emit('line', line)
+			}
+			
+			this.io.emit('cursor', this.cursor = { x, y })
+		}
+		
+		this.onMouseUp = ({ offsetX: x, offsetY: y }) => {
+			this.cursor = { x, y }
+			this.isDrawing = false
+		}
+		
+		this.canvas.addEventListener('mousedown', this.onMouseDown)
+		this.canvas.addEventListener('mousemove', this.onMouseMove)
+		this.canvas.addEventListener('mouseup', this.onMouseUp)
 	}
 	
 	stop = () => {
 		this.io?.close()
-		this.canvas.removeEventListener('mousemove', this.onMouseMoveCallback)
+		this.canvas.removeEventListener('mousedown', this.onMouseDown)
+		this.canvas.removeEventListener('mousemove', this.onMouseMove)
+		this.canvas.removeEventListener('mouseup', this.onMouseUp)
 	}
 	
 	private clear = () => {
@@ -34,9 +77,24 @@ export default class Place {
 		this.context.clearRect(0, 0, width, height)
 	}
 	
-	private drawCursor = ({ x, y }: Cursor) => {
+	private drawCursor = ({ x, y }: Coordinate) => {
 		this.context.beginPath()
 		this.context.arc(x, y, 5, 0, 2 * Math.PI)
 		this.context.fill()
+	}
+	
+	private drawCursors = (cursors: Coordinate[]) => {
+		cursors.forEach(this.drawCursor)
+	}
+	
+	private drawLine = ({ from, to }: Line) => {
+		this.context.beginPath()
+		this.context.moveTo(from.x, from.y)
+		this.context.lineTo(to.x, to.y)
+		this.context.stroke()
+	}
+	
+	private drawLines = () => {
+		this.lines.forEach(this.drawLine)
 	}
 }
