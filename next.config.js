3e614bbab47799e0911f2cdf2e5336f37933581a
Ken Mueller <kenmueller0@gmail.com>
2020-10-29 20:22:58 -0700

    images

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = require('next-compose-plugins')([
+	[require('next-optimized-images')],
+	[require('next-pwa'), {
+		pwa: {
+			disable: process.env.NODE_ENV === 'development',
+			dest: 'public',
+			runtimeCaching: require('next-pwa/cache')
+		}
+	}]
+])
